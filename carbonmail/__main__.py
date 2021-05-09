f3888fd38e70fa6969f652fe0962fae44a3028b0
LucasTARosa <tlucas.23@gmail.com>
2021-05-09 17:22:15 -0300

    Criação da estrutura do projeto

--- carbonmail/__main__.py
+++ carbonmail/__main__.py
@@ -1,3 +1,4 @@
+# Arquivo principal (inicial) a ser executado
+# Quando iniciamos o projeto (carbonmail) ele é o primeiro ao Python executar
+# Nós usamos para ser o ponto de entrada da aplicação
 
-
-print('hello world')
---
+++ carbonmail/database/manager.py
@@ -0,0 +1,2 @@
+# Onde estarão todas as funções deste pacote
+# Ele é quem vai cooderndar este pacote (gerenciador)
